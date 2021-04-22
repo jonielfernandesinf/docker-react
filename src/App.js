a70c414954ad252a08eacbb84ade3a299c1f00c1
Joniel Fernandes <joniel.info@gmail.com>
2021-04-22 11:15:54 -0300

    back tests to work.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changed from the feature branch.
+          Learn React
         </a>
       </header>
     </div>
